13d4458914ca1348d85f87757d889053b3b1a24d
georges tchianga <georgestcm@gmail.com>
2019-08-01 10:46:55 -0400

    sending email method added

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,19 +1,22 @@
+let views = require('../Http/renderController')
+
+const Mail = use('Mail')
 'use strict'
 
 class UserController {
 
-  async store ({ request }) {
-    const data = request.only(['name', 'email', 'message'])
-    const user = await User.create(data)
 
-    await Mail.send('emails.welcome', user.toJSON(), (message) => {
+  async store ({ request,view }) {
+    const data = request.only(['name', 'email', 'message'])
+    await Mail.send('welcome',data,(message) => {
      message
-       .to(user.email)
-       .from('<from-email>')
-       .subject('Welcome to yardstick')
+       .to("tgeorgesdev@gmail.com")
+       .from(data.email)
+       .subject(`New message from ${data.name}`)
    })
-   return 'Registered successfully'
-  }
+
+   return view.render('message')
+}
 }
 
 module.exports = UserController
